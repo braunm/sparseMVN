423ed756e19188ecd02a46880de1d2e1d6d77666
Michael Braun <braunm@smu.edu>
2014-07-30 19:20:22 -0500

    docs to roxygen

--- R/mvnSparse.R
+++ R/mvnSparse.R
@@ -17,6 +17,55 @@
 ##        matrix is sparse, the corresponding covariance
 ##        matrix may not be.
 
+#' @title Multivariate normal functions with sparse covariance/precision matrix.
+#' @aliases dmvn.sparse rmvn.sparse
+#' @description Efficient sampling and density calculation from a multivariate
+#' normal,
+#' when the covariance or precision matrix is sparse. These functions are
+#' designed for MVN samples of very large dimension.
+#'
+#' @param n number of samples
+#' @param x numeric matrix, where each row is an MVN sample.
+#' @param mu mean (numeric vector)
+#' @param CH An object of class dCHMsimpl or dCHMsuper that represents
+#' the Cholesky factorization of either the precision (default) or covariance
+#' matrix.  See details.
+#' @param prec If TRUE, CH is the Cholesky decomposition of the precision
+#' matrix.  If false, it is the decomposition for the covariance matrix.
+#'
+#' @section Details:
+#' These functions uses sparse matrix operations to sample from, or compute the
+#' log density of, a multivariate normal distribution  The user must compute
+#' the Cholesky decomposition first, using the Cholesky function in the Matrix
+#' package.  This function operates on a sparse symmetric matrix, and returns
+#' an object of class dCHMsimpl or dCHMsuper (this depends on the algorithm
+#' that was used for the decomposition).  This object contains information about
+#' any fill-reducing permutations that were used to preserve sparsity.  The
+#' rmvn.sparse and dmvn.sparse functions use this permutation information, even
+#' if pivoting was turned off.
+#'
+#' @examples
+#' if (require(Matrix)) {
+#'    m <- 20
+#'    p <- 2
+#'    k <- 4
+#' 
+#' ## build sample sparse covariance matrix
+#'    Q1 <- tril(kronecker(Matrix(seq(0.1,p,length=p*p),p,p),diag(m)))
+#'    Q2 <- cBind(Q1,Matrix(0,m*p,k))
+#'    Q3 <- rBind(Q2,cBind(Matrix(rnorm(k*m*p),k,m*p),Diagonal(k)))
+#'    V <- tcrossprod(Q3)
+#'    CH <- Cholesky(V)
+#' 
+#'    x <- rmvn.sparse(10,rep(0,p*m+k),CH, FALSE)
+#'    print(x)
+#' 
+#'    y <- dmvn.sparse(x[1,],rep(0,p*m+k), CH, FALSE)
+#'    print(y)
+#' }
+#'
+#' @export
+
 
 rmvn.sparse <- function(n, mu, CH, prec=TRUE) {
 
@@ -29,6 +78,7 @@ rmvn.sparse <- function(n, mu, CH, prec=TRUE) {
     if (!(k>0)) {
         stop("mu must have positive length")
     }
+   
     if (!(n>0)) {
         stop("n must be positive")
     }
---
+++ R/sparseMVN.R
@@ -0,0 +1,13 @@
+#' @title Multivariate normal functions for sparse covariate and precision matrices.
+#'
+#' 
+#' @description Computes multivariate normal (MVN) densities, and samples
+#' from MVN distributions, for sparse covariance and precision matrices.  This
+#' package uses the sparse matrix classes and algorithms in the Matrix package
+#' for computational efficiency.
+#'
+#' @docType package
+#' @name sparseMVN
+#' @import Matrix
+NULL
+
