9e48dac4d4361936667ac32bc5eccdf62838a8d8
Michael Braun <braunm@smu.edu>
2016-12-25 22:54:31 -0600

    started writing a vignette

--- R/sparseMVN.R
+++ R/sparseMVN.R
@@ -2,16 +2,16 @@
 #' @aliases sparseMVN-package sparseMVN
 #' @docType package
 #' @title Multivariate Normal Functions for Sparse Covariate and Precision Matrices
-#'
+#' @description MVN functions for sparse covariance and precision matrices.
 #' @details Computes multivariate normal (MVN) densities, and samples
 #' from MVN distributions, when either the covariance or precision
 #' matrix is stored as a sparse Matrix (a dsCMatrix object, as defined
 #' in the Matrix package.  The user can provide the precision matrix
 #' directly, rather than convert it to a covariance via matrix
 #' inversion.
-#'
-#' @name sparseMVN
 #' @import Matrix
+#' @import methods
+#' @importFrom stats rnorm
 #' @encoding UTF-8
 #' @keywords package
 NULL
