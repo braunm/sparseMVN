38f604e7e72dee2bed316fc10a1176a829f903df
Michael Braun <braunm@smu.edu>
2016-12-30 12:02:56 -0600

    new replication script

--- vignettes/replication.R
+++ vignettes/replication.R
@@ -9,15 +9,14 @@ registerDoParallel(cores=4)
 get_times <- function(D, reps=100) {
 
     s <- D$s ## number of random samples
-    p <-  D$p ## population variables
     k <- D$k ## heterogeneous variables
     N <- D$N ## number of agents
     prec <- D$prec ## 
 
-    mu <- rep(0,k*N + p)  ## assume mean at origin
+    mu <- rep(0,k*N + k)  ## assume mean at origin
     Q1 <- tril(kronecker(Matrix(seq(0.1,k,length=k*k),k,k),diag(N)))
-    Q2 <- cBind(Q1,Matrix(0, N*k, p))
-    Q3 <- rBind(Q2,cBind(Matrix(rnorm(N*k*p), p, N*k), Diagonal(k)))
+    Q2 <- cBind(Q1,Matrix(0, N*k, k))
+    Q3 <- rBind(Q2,cBind(Matrix(rnorm(N*k*k), k, N*k), Diagonal(k)))
     CV.sparse <- tcrossprod(Q3)
     CV.dense <- as(CV.sparse, "matrix")  ## dense covariance
 
@@ -30,12 +29,14 @@ get_times <- function(D, reps=100) {
     stopifnot(all.equal(d.sp,d.dens))
     
     bench <- microbenchmark(
-        chol = Cholesky(CV.sparse),
-        solve = solve(CV.dense),
-        r_sparse = rmvn.sparse(s, mu, chol.CV, prec=prec),
-        d_sparse = dmvn.sparse(x.sp, mu, chol.CV, prec=prec),
-        r_dense = rmvnorm(s, mu, sigma, method="chol"),
-        d_dense = dmvnorm(x.sp, mu, sigma, log=TRUE),
+        chol_sparse = Cholesky(CV.sparse),
+        chol_dense = chol(CV.dense),
+        solve_sparse = solve(CV.sparse),
+        solve_dense = solve(CV.dense),
+        rand_sparse = rmvn.sparse(s, mu, chol.CV, prec=prec),
+        density_sparse = dmvn.sparse(x.sp, mu, chol.CV, prec=prec),
+        rand_dense = rmvnorm(s, mu, sigma, method="chol"),
+        density_dense = dmvnorm(x.sp, mu, sigma, log=TRUE),
         times = reps
     )
 
@@ -44,30 +45,31 @@ get_times <- function(D, reps=100) {
                                                       time=x$time,
                                                       rep=1:length(x$expr))))
     
-    data.frame(s=s, N=N, k=k, p=p, prec=prec, bench=vals)
+    data.frame(s=s, N=N, k=k, prec=prec, bench=vals)
 }
 
 
 reps <- 10
 ## times in milliseconds
-runtimes <- expand.grid(s = c(20, 100),
-                 N = c(5, 10, 15),
-                 k = 3,
-                 p = 3,
-                 prec = c(FALSE, TRUE)) %>%
-    ddply(c("s","N","k","p","prec"), get_times, reps=reps,
-          .parallel=FALSE) %>%
-    group_by(s, N, k, p, prec, bench.expr) %>%
+cases <- expand.grid(s = c(20, 100),
+                     N = c(5, 10, 15),
+                     k = 3,
+                     prec = c(FALSE, TRUE)) %>%
+    mutate(nvars=(N+1)*k,
+           nels = nvars^2,
+           nnz = N*k^2 + k^2 + 2*N*k*k,
+           nnzLT = (N+1) * k*(k+1)/2 + N*k*k,
+           pct.nnz = nnz/nels) 
+
+runtimes <- ddply(cases, c("s","N","k","prec"), get_times, reps=reps,
+                  .parallel=FALSE) %>%
+    group_by(s, N, k, prec, bench.expr) %>%
     summarize(mean = mean(bench.time/10^6),
               sd = sd(bench.time/10^6)) %>%
-    rename(step=bench.expr) %>%
-    mutate(nvars=N*k+p,
-           nels = nvars^2,
-           nnz = N*k^2 + p^2 + 2*N*k*p,
-           nnzLT = N * k*(k+1)/2 + p*(p+1)/2 + p*N*k,
-           pct.nnz = nnz/nels) %>%
-    ungroup()
+    ungroup() %>%
+    tidyr::separate(bench.expr, c("step", "storage"), sep="_")
+    
 
-save(runtimes, file="vignettes/runtimes.Rdata")
+save(cases, runtimes, file="vignettes/runtimes.Rdata")
 
 
